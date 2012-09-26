aa3d18af870418860935cadd97b64cf3ac7d4123
Luc Donnet <luc.donnet@dryade.net>
2012-09-26 11:07:54 +0200

    Add CAPI implementation

--- lib/georuby-ext/rgeo.rb
+++ lib/georuby-ext/rgeo.rb
@@ -1,3 +1,7 @@
+###############################
+#           FFI               #
+###############################
+
 class RGeo::Geos::FFIPointImpl
   def to_georuby
     GeoRuby::SimpleFeatures::Point.from_x_y x, y, srid
@@ -29,6 +33,45 @@ class RGeo::Geos::FFIMultiPolygonImpl
 end
 
 class RGeo::Geos::FFIGeometryCollectionImpl
+  def to_georuby
+    GeoRuby::SimpleFeatures::GeometryCollection.from_geometries collect(&:to_georuby), srid
+  end
+end
+
+###############################
+#           CAPI              #
+###############################
+class RGeo::Geos::CAPIPointImpl
+  def to_georuby
+    GeoRuby::SimpleFeatures::Point.from_x_y x, y, srid
+  end
+end
+
+class RGeo::Geos::CAPILineStringImpl
+  def to_georuby
+    GeoRuby::SimpleFeatures::LineString.from_points points.collect(&:to_georuby), srid
+  end
+end
+
+class RGeo::Geos::CAPILinearRingImpl
+  def to_georuby
+    GeoRuby::SimpleFeatures::LinearRing.from_points points.collect(&:to_georuby), srid
+  end
+end
+
+class RGeo::Geos::CAPIPolygonImpl
+  def to_georuby
+    GeoRuby::SimpleFeatures::Polygon.from_linear_rings [exterior_ring.to_georuby] + interior_rings.map(&:to_georuby), srid
+  end
+end
+
+class RGeo::Geos::CAPIMultiPolygonImpl
+  def to_georuby
+    GeoRuby::SimpleFeatures::MultiPolygon.from_polygons collect(&:to_georuby), srid
+  end
+end
+
+class RGeo::Geos::CAPIGeometryCollectionImpl
   def to_georuby
     GeoRuby::SimpleFeatures::GeometryCollection.from_geometries collect(&:to_georuby), srid
   end
